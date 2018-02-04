12dbbf7cf33e3428849e8d3d1ff75543ee81a54b
No01eg <Novikoff_oleg@tut.by>
2018-02-04 13:22:44 +0300

    Дополнительный класс для передачи пакетов через сокет

---
+++ UserCard/UserCard/CardData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserCard
+{
+  public class CardData
+  {
+    public string cmd { get; set; }
+    private int cmdFieldLenght { get; set; }
+
+    public string dataName { get; set; }
+    private int dataNameFieldLenght { get; set; }
+
+    public string dataPhone { get; set; }
+    private int dataPhoneFieldLenght { get; set; }
+
+    public string dataMail { get; set; }
+    private int dataMailFieldLenght { get; set; }
+
+    public byte[] ToBytes()
+    {
+      //Преобразуем данные в массив байт
+      //byte[] string
+    }
+  }
+}
