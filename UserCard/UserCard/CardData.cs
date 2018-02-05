d8c8523ef4bbcaf73973862f9c991db444ed9e9b
No01eg <Oleg.Novikov@vitebsk.energo.by>
2018-02-05 09:56:43 +0300

    доработана библиотека конвертации

--- UserCard/UserCard/CardData.cs
+++ UserCard/UserCard/CardData.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace UserCard
 {
@@ -20,10 +16,63 @@ namespace UserCard
     public string dataMail { get; set; }
     private int dataMailFieldLenght { get; set; }
 
+    /*
+     * вычисляем значения полей для каждого по отдельности и затем сумма байт
+     */
+    public int GetLenght()
+    {
+      cmdFieldLenght = cmd.Length;
+      dataNameFieldLenght = dataName.Length;
+      dataPhoneFieldLenght = dataPhone.Length;
+      dataMailFieldLenght = dataMail.Length;
+      return cmdFieldLenght + dataMailFieldLenght + dataPhoneFieldLenght + dataMailFieldLenght;
+    }
+
+    /*
+     * Конвертируем объект в массив байт
+     */
     public byte[] ToBytes()
     {
-      //Преобразуем данные в массив байт
-      //byte[] string
+      var Messagedata = new byte[this.GetLenght() + 4 * sizeof(int)]; // длина строк + 4 позиции int для хранения длин строк
+
+      using (var stream = new MemoryStream(Messagedata))
+      {
+        var writer = new BinaryWriter(stream);
+        writer.Write(cmdFieldLenght);//длина команды
+        writer.Write(cmd);//команда
+        writer.Write(dataNameFieldLenght);//длина имени
+        writer.Write(dataName);//имя
+        writer.Write(dataPhoneFieldLenght);
+        writer.Write(dataPhone);
+        writer.Write(dataMailFieldLenght);
+        writer.Write(dataMail);
+        return Messagedata;
+      }
+    }
+
+    /*
+     * Обратная конвертация массива в объект
+     */
+    public CardData FromBytes(byte[] bytes)
+    {
+      using (var ms = new MemoryStream(bytes))
+      {
+        var br = new BinaryReader(ms);
+        var currentObject = new CardData();
+        currentObject.cmdFieldLenght = br.ReadInt32();
+        currentObject.cmd = br.ReadBytes(currentObject.cmdFieldLenght).ToString();
+
+        currentObject.dataNameFieldLenght = br.ReadInt32();
+        currentObject.dataName = br.ReadBytes(currentObject.dataNameFieldLenght).ToString();
+
+        currentObject.dataPhoneFieldLenght = br.ReadInt32();
+        currentObject.dataPhone = br.ReadBytes(currentObject.dataPhoneFieldLenght).ToString();
+
+        currentObject.dataMailFieldLenght = br.ReadInt32();
+        currentObject.dataMail = br.ReadBytes(currentObject.dataMailFieldLenght).ToString();
+
+        return currentObject;
+      }
     }
   }
 }
